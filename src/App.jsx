7c432a0ada83e521a483f6a5fa93d8a22b885b79
BartoszOmiotek16 <bartosomiotek@gmail.com>
2023-06-05 23:17:06 +0200

    modified:   src/App.jsx 	modified:   package-lock.json 	modified:   package.json

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     <h1>Bartosz Omiotek</h1>
       <h3>Gr:6, Nr indeksu: 113969</h3>
       
-      <img src='.logo.png'/>
+      <img src='logo.png'/>
 
       </div>
       </div>
