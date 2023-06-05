3aa7089bd783c5c150e55ddc54ab71f106919b0c
BartoszOmiotek16 <bartosomiotek@gmail.com>
2023-06-05 23:16:26 +0200

    modified:   src/App.jsx modified:   package-lock.json 	modified:   package.json

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     <h1>Bartosz Omiotek</h1>
       <h3>Gr:6, Nr indeksu: 113969</h3>
       
-      <img src={reactLogo} className="logo react" alt="React logo" />
+      <img src='.logo.png'/>
 
       </div>
       </div>
